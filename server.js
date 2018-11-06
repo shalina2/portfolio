08628c2034b1ec2b6c3bed5d0b9b393d40466fd4
shalom belaineh <shalom.belaineh@gmail.com>
2018-11-05 16:34:32 -0800

    added basic server for heroku deployment

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static('./public'));
+app.get('/',(request,response)=> {
+    response.sendFile('index.html',{root: './public'});
+
+});
+
+app.use('*', (request, response)=> response.send('sorry'));
+app.listen(PORT,()=> console.log(`listen ${PORT}`))
+
+
